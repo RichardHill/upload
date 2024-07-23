c6bfba97e300e85f48c368e21a97099740189f61
Richard Hill <richard.hill@wipit.io>
2024-07-23 09:54:34 +0100

    Adding option 12 in

--- app/page.tsx
+++ app/page.tsx
@@ -335,7 +335,7 @@ export default function Home() {
 //     return dictionary[text]
 // }
 
-type OptionType = "option1" | "option2" | "option3" | "option4" | "option5" | "option6" | "option7" | "option8" | "option9" | "option10" | "option11"
+type OptionType = "option1" | "option2" | "option3" | "option4" | "option5" | "option6" | "option7" | "option8" | "option9" | "option10" | "option11" | "option12"
 function getEndpoint(option: OptionType) {
 
     const endpoints = {
